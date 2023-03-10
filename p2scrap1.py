cb21d277c579a3af4b82aa8758a60d98710501b7
CeeG33 <114352497+CeeG33@users.noreply.github.com>
2023-03-10 19:53:22 +0100

    Extraction partielle de données d'un livre

---
+++ p2scrap1.py
@@ -0,0 +1,30 @@
+# importation des packages requis
+import requests
+from bs4 import BeautifulSoup
+
+#définition de l'url
+product_page_url = "http://books.toscrape.com/catalogue/the-black-maria_991/index.html"
+
+#appel de l'url
+product_page_html = requests.get(product_page_url)
+
+#parsage des données
+html_soup = BeautifulSoup(product_page_html.content, 'html.parser')
+
+#trouve toutes les cellules du tableau contenant les principales informations concernant le livre (extract)
+infos = html_soup.find_all('td')
+
+#transforme les données récoltées en liste (transform)
+infos_livres = []
+for info in infos:
+    infos_livres.append(info.string)
+
+#extrait le titre du livre
+titre_livre = html_soup.find('li', class_='active').string
+
+
+print(product_page_url)
+print()
+print(infos_livres)
+print()
+print(titre_livre)
