a8d4ff9ba359015131698377e1c10385d78a337f
CeeG33 <114352497+CeeG33@users.noreply.github.com>
2023-03-14 12:34:52 +0100

    Ajout extraction URL + optimisation du code

--- p2scrap1.py
+++ p2scrap1.py
@@ -1,35 +1,38 @@
-# importation des packages requis
+# Importation des packages requis
 import requests
 from bs4 import BeautifulSoup
 
-#définition de l'url
+# Définition de l'url
 product_page_url = "http://books.toscrape.com/catalogue/the-black-maria_991/index.html"
 
-#appel de l'url
+# Appel de l'url
 product_page_html = requests.get(product_page_url)
 
-#parsage des données
+# Parsage des données
 html_soup = BeautifulSoup(product_page_html.content, 'html.parser')
 
-#trouve toutes les cellules du tableau contenant les principales informations concernant le livre (extract)
+# Trouve toutes les cellules du tableau contenant les principales informations concernant le livre (extract)
 infos = html_soup.find_all('td')
 
-#transforme les données récoltées en liste (transform)
-infos_livres = []
-for info in infos:
-    infos_livres.append(info.string)
+# Extrait
+tableau = html_soup.find('table', class_='table table-striped')
+tableau_td = tableau.find_all('td')
+upc = tableau_td[0].text
+priceExclTax = tableau_td[2].text
+priceInclTax = tableau_td[3].text
+availability = tableau_td[5].text
 
-#extrait le titre du livre
+# Extrait le titre du livre (extract)
 #titre_livre = html_soup.find('li', class_='active').string
 ul_elements = html_soup.find('ul', class_='breadcrumb')
 li_elements = ul_elements.find_all('li')
 cat_livre = li_elements[2]
 titre_livre2 = li_elements[3]
 
-#extrait la description du livre
+# Extrait la description du livre (extract)
 description_livre = html_soup.find_all('p')[3].string
 
-#notation du livre
+# Extrait la note du livre (extract)
 div_livre = html_soup.find('div', class_='col-sm-6 product_main')
 notation1 = div_livre.find('p', class_='star-rating One')
 notation2 = div_livre.find('p', class_='star-rating Two')
@@ -48,35 +51,28 @@ elif notation4:
 elif notation5:
       note_livre = '5/5'
 
-            
+# Extrait l'URL de l'image de couverture (extract)
+div_img = html_soup.find('div', class_='item active')
+src = div_img.find('img')['src']
+img_url = 'http://books.toscrape.com/' + src.replace('../../', '') 
 
-"""
-note1 = html_soup.find('p', class_='star-rating One')
-if note1: 
-        print('1/5')
 
-note2 = html_soup.find('p', class_='star-rating Two')
-if note2: 
-        print('2/5')
-
-note3 = html_soup.find('p', class_='star-rating Three')
-if note3: 
-        print('3/5')
-
-note4 = html_soup.find('p', class_='star-rating Four')
-if note4: 
-        print('4/5')
-
-note5 = html_soup.find('p', class_='star-rating Five')
-if note5: 
-        print('5/5')
-"""
-#print(product_page_url)
+print(product_page_url)
 print()
-#print(infos_livres)
+print(upc)
 print()
-#print(titre_livre2.text)
+print(titre_livre2.text)
 print()
-#print(description_livre)
-#print(cat_livre.text)
+print(priceInclTax)
+print()
+print(priceExclTax)
+print()
+print(availability)
+print()
+print(description_livre)
+print()
+print(cat_livre.text)
+print()
 print(note_livre)
+print()
+print(img_url)
