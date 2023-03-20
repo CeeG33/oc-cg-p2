dec18af46a4b3686bcae757cc56686b5e3979b1e
CeeG33 <114352497+CeeG33@users.noreply.github.com>
2023-03-20 13:16:02 +0100

    Ajout du code pour extraire l'image du livre

--- p2scrap1.py
+++ p2scrap1.py
@@ -5,7 +5,7 @@ import os
 
 # Début Phase 1
 
-product_page_url = "http://books.toscrape.com/catalogue/the-black-maria_991/index.html"
+product_page_url = "http://books.toscrape.com/catalogue/the-faith-of-christopher-hitchens-the-restless-soul-of-the-worlds-most-notorious-atheist_495/index.html"
 
 def req_html(product_page_url):
       return requests.get(product_page_url)
@@ -51,12 +51,18 @@ def book_data(product_page_url):
             elif 'Five' in mark['class']:
                   book_mark = '5_5'
 
-      # Extrait l'URL de l'image de couverture (extract)
+      # Extrait l'URL de l'image de couverture (extract) et la télécharge
       div_img = html_soup.find('div', class_='item active')
       src = div_img.find('img')['src']
       img_url = 'http://books.toscrape.com/' + src.replace('../../', '') 
-
-      
+      img_directory = 'books_img/' + cat_book.replace(' ', '') + '/' + upc.replace(' ', '') + '/'
+      img_title = upc.replace(' ', '') + '.jpg'
+      if not os.path.exists(img_directory): 
+            os.makedirs(img_directory)
+      response_img = requests.get(img_url)
+      with open(img_directory + img_title, 'wb') as f:
+            f.write(response_img.content) 
+     
       data_list = [
             [product_page_url, 
             upc,
@@ -83,7 +89,8 @@ def book_data(product_page_url):
             'image_url'
       ]
 
-      # Création du fichier CSV
+book_data(product_page_url)
+"""
       csv_file_name = 'books' + cat_book.replace(' ', '') + '.csv'
       
       with open(csv_file_name, 'a', encoding="utf-8", newline='') as csv_file:
@@ -94,5 +101,5 @@ def book_data(product_page_url):
             else:
                   writer = csv.writer(csv_file, delimiter=',')
                   writer.writerows(data_list)
-      
-      
+
+"""
